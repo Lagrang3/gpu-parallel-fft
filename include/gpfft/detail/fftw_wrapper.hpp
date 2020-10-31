d20b2f99f4b4f30f43a93d488d962cec0dfbff3b
Lagrang3 <lagrang3@protonmail.com>
2020-10-31 09:00:36 +0100

    added clang-format target

--- include/gpfft/detail/fftw_wrapper.hpp
+++ include/gpfft/detail/fftw_wrapper.hpp
@@ -2,19 +2,22 @@
 /*
     Wrapper to FFTW3 library.
 */
-    
-#include <vector>
+
 #include <complex>
 #include <fftw3.h>
+#include <vector>
 
 namespace gpfft
 {
+    enum class FFT_type
+    {
+        forward,
+        backward
+    };
 
-enum class FFT_type { forward,backward };
-    
     int FFTW_sign(FFT_type T)
     {
-        switch(T)
+        switch (T)
         {
             case FFT_type::forward:
                 return FFTW_FORWARD;
@@ -23,33 +26,32 @@ enum class FFT_type { forward,backward };
         }
         return -1;
     }
-    
-    template<FFT_type T>
-    std::vector< std::complex<double> > FFTW3(const std::vector< std::complex<double> >& A){
-        const int n=A.size();
+
+    template <FFT_type T>
+    std::vector<std::complex<double>> FFTW3(
+        const std::vector<std::complex<double>>& A)
+    {
+        const int n = A.size();
         fftw_plan plan;
-        fftw_complex *data;
-        
-        data=(fftw_complex*)fftw_malloc(sizeof(fftw_complex)*n);
-        plan=fftw_plan_dft_1d(n,data,data,FFTW_sign(T), FFTW_ESTIMATE  );
-        
-        for(int i=0;i<n;++i)
-            data[i][0]=A[i].real(),
-            data[i][1]=A[i].imag();
-        
+        fftw_complex* data;
+
+        data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * n);
+        plan = fftw_plan_dft_1d(n, data, data, FFTW_sign(T), FFTW_ESTIMATE);
+
+        for (int i = 0; i < n; ++i)
+            data[i][0] = A[i].real(), data[i][1] = A[i].imag();
+
         fftw_execute(plan);
-        
-        std::vector< std::complex<double> > B(n);
-        
-        for(int i=0;i<n;++i)
-            B[i]={data[i][0],data[i][1]};
-        
-        
+
+        std::vector<std::complex<double>> B(n);
+
+        for (int i = 0; i < n; ++i)
+            B[i] = {data[i][0], data[i][1]};
+
         fftw_free(data);
         fftw_destroy_plan(plan);
-            
+
         return B;
-            
     }
 
-}
+}  // namespace gpfft
--- tests/1d-fftw.cpp
+++ tests/1d-fftw.cpp
@@ -1,10 +1,10 @@
-#define BOOST_TEST_MODULE 1D-FFTW-Tests
+#define BOOST_TEST_MODULE 1D - FFTW - Tests
 
 #include <boost/test/unit_test.hpp>
+#include <cmath>
+#include <complex>
 #include <gpfft/detail/fftw_wrapper.hpp>
 #include <vector>
-#include <complex>
-#include <cmath>
 
 using cd = std::complex<double>;
 using gpfft::FFT_type;
@@ -13,26 +13,26 @@ BOOST_AUTO_TEST_CASE(small_transforms)
 {
     const double pi = acos(-1.0);
     std::vector<cd> out;
-    
+
     out = gpfft::FFTW3<FFT_type::forward>({3});
-    BOOST_CHECK(out.size()==1);
-    BOOST_CHECK_SMALL(std::abs(out[0]-cd{3,0}),1e-12);
-    
+    BOOST_CHECK(out.size() == 1);
+    BOOST_CHECK_SMALL(std::abs(out[0] - cd{3, 0}), 1e-12);
+
     out = gpfft::FFTW3<FFT_type::backward>({3});
-    BOOST_CHECK(out.size()==1);
-    BOOST_CHECK_SMALL(std::abs(out[0]-cd{3,0}),1e-12);
-    
-    out = gpfft::FFTW3<FFT_type::forward>({1,1});
-    BOOST_CHECK(out.size()==2);
-    BOOST_CHECK_SMALL(std::abs(out[0]-cd{2,0}),1e-12);
-    BOOST_CHECK_SMALL(std::abs(out[1]-cd{0,0}),1e-12);
-    
+    BOOST_CHECK(out.size() == 1);
+    BOOST_CHECK_SMALL(std::abs(out[0] - cd{3, 0}), 1e-12);
+
+    out = gpfft::FFTW3<FFT_type::forward>({1, 1});
+    BOOST_CHECK(out.size() == 2);
+    BOOST_CHECK_SMALL(std::abs(out[0] - cd{2, 0}), 1e-12);
+    BOOST_CHECK_SMALL(std::abs(out[1] - cd{0, 0}), 1e-12);
+
     {
-    out = gpfft::FFTW3<FFT_type::forward>({1,1,1});
-    cd w{ cos(2*pi/3),sin(2*pi/3) },w2=w*w;
-    BOOST_CHECK(out.size()==3);
-    BOOST_CHECK_SMALL(std::abs(out[0]-cd{3,0}),1e-12);
-    BOOST_CHECK_SMALL(std::abs(out[1]-1.-w-w2),1e-12);
-    BOOST_CHECK_SMALL(std::abs(out[2]-1.-w2 - w2*w2),1e-12);
+        out = gpfft::FFTW3<FFT_type::forward>({1, 1, 1});
+        cd w{cos(2 * pi / 3), sin(2 * pi / 3)}, w2 = w * w;
+        BOOST_CHECK(out.size() == 3);
+        BOOST_CHECK_SMALL(std::abs(out[0] - cd{3, 0}), 1e-12);
+        BOOST_CHECK_SMALL(std::abs(out[1] - 1. - w - w2), 1e-12);
+        BOOST_CHECK_SMALL(std::abs(out[2] - 1. - w2 - w2 * w2), 1e-12);
     }
 }
